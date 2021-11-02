4f2d2c9d6d541983e4b94f618a4c3a8ee78f010e
gaowenyi97 <1170374179@qq.com>
2021-11-02 14:10:49 +0800

    five

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
   user: '1170374179@qq.com', // 发送者邮箱
   pass: 'ebvbgsiwcwoygjjf', // 发送者邮箱MTP协议密码
-  to: '1170374179@qq.com', // 发送到谁，填邮箱
+  to: '591692335@qq.com', // 发送到谁，填邮箱
   weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
   location: '101020600',  // 和风天气-浦东新区的id
   type: '1,3,9,15', // 和风天气-生活指数type
